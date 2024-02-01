1e74d395e852574e7682fe4deffc58201f377fa5
hassan <sudane@gmail.com>
2024-02-01 12:38:03 +0000

    uupdate

---
+++ src/routes/Nav.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+    let navs = [
+        {
+        url:'/',
+        name: 'Home'
+    },
+    {
+        url:'/quotes',
+        name: 'Quotes'
+    },
+    {
+        url:'/shop',
+        name: 'Shops'
+    },
+    {
+        url:'/movies',
+        name: 'Movies'
+    },
+    {
+        url:'/users',
+        name: 'Users'
+    }]
+</script>
+<div class="center" id="navbarNav">
+<ul class="nav nav-pills justify-content-center">
+    {#each navs as nav}
+    <li class="nav-item" aria-current={$page.url.pathname === nav.url ? 'page' : undefined}>
+        <a class="nav-link {$page.url.pathname === nav.url ? 'active' : undefined}" href="{nav.url}">{nav.name}</a>
+      </li>
+    {/each}   
+</ul>
+</div>
+
--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,32 +1,50 @@
 <script>
-    export let data
-    let pageSize = 10
-    $: totalItems = data.users.total 
-    $: totalPages = Math.ceil(totalItems / pageSize)
- 
+  export let data;
+  let pageSize = 10;
+  let currentPage = 1;  
+  $: totalItems = data.users.total;
+  $: totalPages = Math.ceil(totalItems / pageSize);
+  function itemSelected(value) {
+    alert(value.email);
+  }
 </script>
+
 <h1>Users</h1>
 <table class="table">
-    <thead>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">User Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each data.users.users as user}
       <tr>
-        <th scope="col">#</th>
-        <th scope="col">User Email</th>        
+        <th scope="row">{user.id}</th>
+        <td>{user.email}</td>
+        <td class="center">
+          <button
+            type="button"
+            class="btn btn-primary btn-sm"
+            on:click={() => itemSelected(user)}>Select</button
+          >
+        </td>
       </tr>
-    </thead>
-    <tbody> 
-        {#each data.users.users as  user}
-      <tr>
-      <th scope="row">{user.id}</th>
-       <td>{user.email}</td>        
-      </tr>
-      {/each}
-    </tbody>
-  </table>  
+    {/each}
+  </tbody>
+</table>
 
-  <nav aria-label="Page navigation example">
-    <ul class="pagination">
-        {#each Array(totalPages) as _, idx}        
-       <li class="page-item"><a class="page-link" href="/users?limit={pageSize}&skip={pageSize * idx}">{idx + 1}</a></li>
-       {/each}
-    </ul>
-  </nav>
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    {#each Array(totalPages) as _, idx}        
+      <li class="page-item" >
+        <a
+          on:click={() => (currentPage = idx + 1)}
+          class="page-link {currentPage == idx + 1 ? 'active' : undefined}" 
+          href="/users?limit={pageSize}&skip={pageSize * idx}">{idx + 1}</a
+        >
+      </li>
+    {/each}
+  </ul>
+</nav>
